eb190f6721ddcdd274d89e253081904102fbd1bc
Atchayam G <atchayamganesh@gmail.com>
2026-08-02 06:27:47 +0530

    release: publish M2 UI demo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "753461446"
+    serviceWorkerVersion: "1710144757"
   }
 });
